1b199dc44f6290aea887d2cd09fcfed50be0015c
Dominika Gnich <d.gnich@wp.pl>
2017-06-10 13:28:48 +0200

    while

---
+++ js/loops.js
@@ -0,0 +1,19 @@
+'use strict';
+//PĘTLE- pozwalają wykonać dany kod pewną ilość razy//
+
+//są 2 typy pętli: for i while //
+
+//for ( var i=4; i> -1; i--){
+//    console.log(i);
+//}
+
+//for używamy gdy znamy liczbę powtórzeń, a gdy jej nie znamy używamy while//
+
+//while//
+
+var it= 0;
+while ( it < 15) {
+    console.log(it);
+    it++;
+}
+
